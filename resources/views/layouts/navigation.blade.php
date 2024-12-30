55d80791fe9774e2cb2fda0c6ad41492c8aab13b
AISYAH SITI ZAHRA <160479611+aszahra@users.noreply.github.com>
2024-12-31 01:28:29 +0700

    add navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -56,6 +56,11 @@
                         {{ __('Transaksi') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Laporan') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
