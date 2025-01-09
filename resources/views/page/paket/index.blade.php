dcc0886287a0085ea854755834e60e28c7ca2d03
AISYAH SITI ZAHRA <160479611+aszahra@users.noreply.github.com>
2025-01-09 23:44:42 +0700

    change nama_paket

--- resources/views/page/paket/index.blade.php
+++ resources/views/page/paket/index.blade.php
@@ -48,12 +48,15 @@
                                 <label for="nama_paket"
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
                                     Paket</label>
-                                <select class="js-example-placeholder-single js-states form-control w-full"
+                                <input type="text" id="nama_paket" name="nama_paket"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Masukan nama paket disini...">
+                                {{-- <select class="js-example-placeholder-single js-states form-control w-full"
                                     name="nama_paket" data-placeholder="Pilih Paket">
                                     <option value="" disabled selected>Pilih...</option>
                                     <option value="ONEDAY">SATU HARI</option>
                                     <option value="KILAT">KILAT</option>
-                                </select>
+                                </select> --}}
                             </div>
                             <button type="submit"
                                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">SIMPAN</button>
